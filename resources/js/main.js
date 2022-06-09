77263f7d47f7db9c6d728ec084f8ce8dbf38c62c
Neonie Quell <neoniequell.ponce@yahoo.com>
2022-06-09 13:24:27 +0800

    Added Initial Profile Page and Configured Logo Name Link to Home Page On Click

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,11 +1,19 @@
-const menuBtn = document.querySelector(".nav-menu-btn");
-const closeBtn = document.querySelector(".nav-close-btn");
-const navigation = document.querySelector(".navigation");
+const menuBtn = document.querySelector('.nav-menu-btn');
+const closeBtn = document.querySelector('.nav-close-btn');
+const navigation = document.querySelector('.navigation');
 
-menuBtn.addEventListener("click", () => {
-    navigation.classList.add("active");
+menuBtn.addEventListener('click', () => {
+    navigation.classList.add('active');
 });
 
-closeBtn.addEventListener("click", () => {
-    navigation.classList.remove("active");
+closeBtn.addEventListener('click', () => {
+    navigation.classList.remove('active');
 });
+
+function onLogoClick(curPage) {
+    if (curPage === 'index') {
+        location.href="index.html";
+    } else {
+        location.href="../../index.html";
+    }
+}
